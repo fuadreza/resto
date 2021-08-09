bd4d27f6643ad7162b99f27ad8c91be8c83d6be9
fuadreza <fuadreza1011@gmail.com>
2021-08-10 01:31:22 +0700

    add setting layout

--- lib/feature/resto/presentation/pages/setting/setting_page.dart
+++ lib/feature/resto/presentation/pages/setting/setting_page.dart
@@ -1,11 +1,73 @@
-
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class SettingPage extends StatelessWidget {
-  const SettingPage({Key? key}) : super(key: key);
+class SettingPage extends StatefulWidget {
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  var isNotificationEnable = false;
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Setting'));
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.only(
+            top: 40,
+            left: 16,
+            right: 16,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Setting',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Restaurant Notification',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Enable notification',
+                        ),
+                      ],
+                    ),
+                  ),
+                  Wrap(
+                    children: [
+                      Switch(
+                        value: isNotificationEnable,
+                        onChanged: (value) {
+                          setState(() {
+                            isNotificationEnable = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
